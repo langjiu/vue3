32005c16c2d8e2c0edfdc9c38cba77cf00e7dc7e
haibo.liu1 <haibo.liu1@chinacreator.com>
2022-08-14 17:49:49 +0800

    basic项目

--- vue3_demo/src/components/Blog.vue
+++ vue3_demo/src/components/Blog.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <router-link to="/"><button>返回首页</button></router-link>
-    <h1>博客详情：{{$route.params.id}}</h1>
-    <h1>{{blogCountent}}</h1>
-    </div>
+    <h1>博客详情：{{ $route.params.id }}</h1>
+    <h1>{{ blogCountent }}</h1>
+  </div>
 </template>
 
 <script>
@@ -11,8 +11,8 @@ export default {
   components: {},
   data() {
     return {
-      blogCountent:"",
-    }
+      blogCountent: "",
+    };
   },
   created() {
     let id = this.$route.params.id;
@@ -20,16 +20,26 @@ export default {
   },
   methods: {
     getBlogDataById(id) {
-      switch(id) {
-        case "1":this.blogCountent = "切图必备技能";break;
-        case "2":this.blogCountent = "javaScript很重要";break;
-        case "3":this.blogCountent = "node前端开发运行环境";break;
-        case "4":this.blogCountent = "前后台数据交互";break;
-        case "5":this.blogCountent = "组件化模块开发";break;
+      switch (id) {
+        case "1":
+          this.blogCountent = "切图必备技能";
+          break;
+        case "2":
+          this.blogCountent = "javaScript很重要";
+          break;
+        case "3":
+          this.blogCountent = "node前端开发运行环境";
+          break;
+        case "4":
+          this.blogCountent = "前后台数据交互";
+          break;
+        case "5":
+          this.blogCountent = "组件化模块开发";
+          break;
       }
-    }
+    },
   },
-}
+};
 </script>
 
 <style></style>
